06a4954458ddb75b6a078ec23423935a9abf71cc
doriver <tkdduq241@naver.com>
2021-11-16 00:59:36 +0900

    11-15

---
+++ src/note/Note01.java
@@ -0,0 +1,12 @@
+package note;
+
+public class Note01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1234 % 10); // 4 , 10으로 나눈 나머지
+		System.out.println(1234 / 10); // 123 , 10으로 나눈 몫
+		
+	}
+
+}
