4402725608e420526baa70e9c06e6fa9c59ca3a2
doriver <tkdduq241@naver.com>
2022-07-20 20:13:27 +0900

    ..

---
+++ src/chapter2_variable/Content01.java
@@ -0,0 +1,27 @@
+package chapter2_variable;
+
+public class Content01 {
+
+	public static void main(String args[]) {
+		
+		// 1. Console창에 글자를 출력할때 , main method 안에 System.out.print(); 을 이용
+		System.out.println("hello");
+		System.out.println(3+5);
+		
+		System.out.print(5/3);
+		
+		// 2. 변수
+		int x;
+		x = 5;
+		
+		int y = 3;
+		
+		int a = 100;
+		double pi = 3.14;
+		char ch = 'a';
+		String str = "abc";
+		
+		
+	}
+	
+}
