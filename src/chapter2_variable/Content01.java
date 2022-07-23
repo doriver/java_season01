ef36b83b1a47829bafda083f0f864019ffc5a4f0
doriver <tkdduq241@naver.com>
2022-07-23 16:51:37 +0900

    객체의 생성

--- src/chapter2_variable/Content01.java
+++ src/chapter2_variable/Content01.java
@@ -9,7 +9,7 @@ public class Content01 {
 		System.out.println(3+5);
 		
 		System.out.print(5/3);
-		
+		System.out.println("");
 		// 2. 변수
 		int x;
 		x = 5;
@@ -20,6 +20,22 @@ public class Content01 {
 		double pi = 3.14;
 		char ch = 'a';
 		String str = "abc";
+		
+		// 3. 상수
+		final int MAX_VALUE;
+		MAX_VALUE = 100;
+//		MAX_VALUE = 200; 오류
+
+		// 두 변수의 값 바꾸기
+		int z = 10;
+		int k = 20;
+		
+		int tmp;
+		
+		tmp = z;
+		z = k;
+		k = tmp;
+		System.out.print(z + ", " + k);
 		
 		
 	}
---
+++ src/chapter6_oop1/Content01.java
@@ -0,0 +1,32 @@
+package chapter6_oop1;
+
+// 하나의 소스파일( *.java )에 둘 이상의 클래스를 정의하는것도 가능하다. 
+// 소스파일 이름은 public class의 이름과 일치해야 한다
+// public class가 없다면 소스파일 이름은 소스파일내의 어떤 클래스의 이름으로 해도 상관없다.
+
+// 클래스파일( *.class )은 클래스마다 하나씩 만들어지므로
+// Content01.java를 컴파일 하면 Content01.class와 Tv.class 2개의 클래스파일이 생성된다
+
+public class Content01 {
+	
+	public static void main(String[] args) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("현재 채널은 " + t.channel + " 입니다");
+	}
+	
+}
+
+class Tv {
+	
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() { power = !power; }
+	void channelUp() { ++channel; }
+	void channelDown() { --channel; }
+	
+}
