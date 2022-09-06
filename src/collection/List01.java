4d8fc87dddd67354e302ea36948bb09856ed087f
doriver <tkdduq241@naver.com>
2022-09-06 21:47:43 +0900

    ArrayList 복습 조금함

--- src/collection/List01.java
+++ src/collection/List01.java
@@ -8,21 +8,28 @@ public class List01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		List 는 index가 있다
+//	 	List
+//	 	index가 있다
+//		배열과 같이 같은 타입의 여러 개의 자료를 저장하는 자료구조.
+//		배열이 가진 메모리 낭비와 삽입 삭제의 불편함을 개선하였다.
+		
+		
 		ArrayList<Integer> integerList = new ArrayList<Integer>();
 		integerList.add(4);
 		integerList.add(8);
-		System.out.println(integerList); // [4, 8]
+		integerList.add(7);
+		integerList.add(3);  // add한 순서대로 저장됨
+		System.out.println(integerList); // [4, 8, 7, 3]  ,  참조변수를 println했더니 이렇게 나오네, 보통은 주소값 나옴
 		
 		integerList.add(1, 5); // index 1번에 5를 추가
-		System.out.println(integerList); // [4, 5, 8]
+		System.out.println(integerList); // [4, 5, 8, 7, 3]
 		System.out.println(integerList.get(0)); // 4 (index 0번 값)
 		
 		integerList.set(0, 10); // index 0번값을 10으로 바꾼다
-		System.out.println(integerList); // [10, 5, 8]
+		System.out.println(integerList); // [10, 5, 8, 7, 3]
 		
 		Integer deleteNumber = integerList.remove(0); // index 0번값을 제거한다. 그에 따라 index가 자동으로 마춰짐, 그리고 제거된 값이 결과값이 됨. 
-		System.out.println(integerList); // [5, 8]
+		System.out.println(integerList); // [5, 8, 7, 3]
 		System.out.println(deleteNumber); // 10
 		
 		integerList.clear(); // list의 모든 값을 제거
