cb18d17771136e5a631ec8c0fbde4e334a155e05
JINJU\and or <tkdduq241@naver.com>
2022-09-12 02:18:08 +0900

    for문

--- src/chapter04/if_switch_for_while_statement/Content02.java
+++ src/chapter04/if_switch_for_while_statement/Content02.java
@@ -4,6 +4,37 @@ public class Content02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + ",");
+		}
+		System.out.println();
+		
+		for (int i =1; i <= 10; i+=2) {
+			System.out.print(i + ",");
+		}
+		System.out.println();
+		
+		for (int i = 1; i <= 10; i*=3) {
+			System.out.print(i + ",");
+		}
+		System.out.println();
+		
+		
+		for (int i = 1; i <= 5; i++) {
+			for(int j=1; j <= 3; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 
 	}
 
