98703ba70aa2466e9be0c50474a57b50b1675545
doriver <tkdduq241@naver.com>
2022-09-27 20:30:19 +0900

    super()

--- src/chapter07/object_oriented_programming_2/Content03.java
+++ src/chapter07/object_oriented_programming_2/Content03.java
@@ -9,9 +9,39 @@ public class Content03 {
 		
 		Child2 c2 = new Child2();
 		c2.method(); // 10 , 10 , 10
+		
+		Point3D2 p = new Point3D2(1, 2, 3);
+		System.out.println(p.x + " , " + p.y + " , " + p.z);
+		// 1 , 2 , 3
+	
 	}
 
 }
+
+class Point2 {
+	int x, y;
+	
+	Point2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Point3D2 extends Point2{
+	int z;
+	
+//	Point3D2(int x, int y, int z) { // 에러남
+//		this.x = x; // 조상의 멤버를 초기화
+//		this.y = y; // 조상의 멤버를 초기화
+//		this.z = z;
+//	}
+	Point3D2(int x, int y, int z) {
+		super(x, y); // 조상 클래스의 생성자 Point2(int x, int y)를 호출
+		this.z = z;
+	}
+}
+
+
 
 class Parent1 {
 	int x = 10;
